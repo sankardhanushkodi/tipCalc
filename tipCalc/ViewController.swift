443dd63c45ff1bf8a368378461d2e6ecb77f3c3b
Sankar Dhanushkodi <sankardhanushkodi@gmail.com>
2017-03-05 23:01:56 -0800

    Completed slider control with integer increments to select tip

--- tipCalc/ViewController.swift
+++ tipCalc/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountText: UITextField!
+
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var customTipText: UITextField!
+    
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
 
+    @IBAction func recalcTip(_ sender: Any) {
+        tipSlider.setValue(Float(lroundf(tipSlider.value)), animated: true)
+        tipPercentLabel.text = String(format: "%.0f", tipSlider.value)
+        return calculateTip(sender)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        print("Amount is \(amountText.text)")
+        let amount = Float(amountText.text!) ?? 0
+        let tip = amount * tipSlider.value / 100
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", tip+amount)
+    }
 }
 
